b8da3ab516be64b704a8200f2335bb057b1d15b9
ymittal <yash_mittal@intuit.com>
2023-08-14 17:30:38 +0530

    allign nav items to centre

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -3,14 +3,14 @@ import Link from "next/link";
 import { adminNavOptions, navOptions } from "@/app/Constants";
 import { usePathname, useRouter } from "next/navigation";
 
-function NavItems() {
+function NavItems({router}) {
   return (
-    <div className="w-full lg:inline-flex lg:flex-grow lg:w-auto">
+    <div className="w-full lg:inline-flex lg:w-auto">
       <ul className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
         {navOptions.map((item) => (
           <li
             key={item.id}
-            className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-green-600 hover:text-black"
+            className="lg:inline-flex lg:w-auto w-full px-3 py-3 rounded text-black font-bold items-center justify-center hover:bg-black hover:text-white"
             onClick={() => router.push(item.path)}
           >
             {item.label}
@@ -25,11 +25,11 @@ export default function Navbar() {
   const router = useRouter();
   const pathname = usePathname();
   const isAuthenticated = false;
-  const isAdmin = false;
+  const isAdmin = true;
 
   return (
     <>
-      <nav className="flex items-center flex-wrap bg-white p-3">
+      <nav className="flex items-center flex-wrap justify-between bg-white p-3">
         <div
           className="flex items-center flex-shrink-0 text-black mr-6"
           onClick={() => router.push("/")}
@@ -38,27 +38,24 @@ export default function Navbar() {
             E-Commerce
           </span>
         </div>
-        <div className="block flex">
-          <button
-            className={`flex items-center px-3 py-2 border rounded text-black border-white hover:text-black hover:border-white`}
-            onClick={() => router.push("/account")}
-          >
-            Account
-          </button>
-          <button
-            className={`flex items-center px-3 py-2 border rounded text-black border-white hover:text-black hover:border-white`}
-            onClick={() => router.push("/cart")}
-          >
-            Cart
-          </button>
-          {isAuthenticated ? (
-            <button
-              className={`flex items-center px-3 py-2 border rounded text-black border-white hover:text-black hover:border-white`}
-              onClick={() => router.push("/cart")}
-            >
-              Logout
-            </button>
-          ) : (
+        <div className="flex md:order-2 gap-2">
+          {!isAdmin && (
+              <button
+                className={`flex items-center px-3 py-2 border rounded text-black border-white hover:text-black hover:border-white`}
+                onClick={() => router.push("/cart")}
+              >
+                Cart
+              </button>
+          )}
+          {isAuthenticated && (
+              <button
+                className={`flex items-center px-3 py-2 border rounded text-black border-white hover:text-black hover:border-white`}
+                onClick={() => router.push("/account")}
+              >
+                Account
+              </button>
+          )}
+          {!isAuthenticated && (
             <button
               className={`flex items-center px-3 py-2 border rounded text-black border-white hover:text-black hover:border-white`}
               onClick={() => router.push("/cart")}
@@ -67,6 +64,9 @@ export default function Navbar() {
             </button>
           )}
         </div>
+        <NavItems 
+          router={router}
+        />
       </nav>
     </>
   );
