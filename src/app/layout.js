6ee6a5664c591bf3b07a31c12fb4b61d9fb9f731
ymittal <yash_mittal@intuit.com>
2023-08-14 23:23:53 +0530

    redux setup for login

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,23 @@
+"use client";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/Components/Navbar";
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "E-Commerce",
-  description: "E commerce web app built with next.js",
-};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true} className={inter.className}>
-        <Navbar />
-        {children}
-        {/* <Footer /> */}
-      </body>
+      <Provider store={store}>
+        <body suppressHydrationWarning={true} className={inter.className}>
+          <Navbar />
+          {children}
+          {/* <Footer /> */}
+        </body>
+      </Provider>
     </html>
+  
   );
 }
